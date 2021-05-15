9a7710c54f7a8e3e35cd79049cfa00f11ab79a26
Buse Şentürk <buse.senturk@outlook.com>
2021-05-15 14:23:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions here
+function map (srcArr, func) {
+  let response = []
+  for (let i = 0; i < srcArr.length; i++) {
+      let element = srcArr[i]
+      response.push(func(element))
+    }
+
+    return response;
+  }
