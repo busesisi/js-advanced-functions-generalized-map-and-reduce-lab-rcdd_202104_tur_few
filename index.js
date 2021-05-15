6aaa21e11ce330691a9fe06dc218e9decd944c5e
Buse Şentürk <buse.senturk@outlook.com>
2021-05-15 14:26:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,33 @@ function map (srcArr, func) {
   let response = []
   for (let i = 0; i < srcArr.length; i++) {
       let element = srcArr[i]
-      response.push(func(element))
+      response.push(func(srcArr[i]));
     }
 
     return response;
   }
+/*
+function map (array, func) {
+    let r = [];
+    for (let i = 0; i < array.length; i++ ) {
+      r.push(func(array[i]));
+    }
+    return r;
+}
+
+function reduce (array, func, startingPoint = 0) {
+	let newValue = startingPoint;
+    for (let i = 0; i < array.length; i++ ) {
+        if(array[i] === false){
+            newValue = false;
+            continue
+        } else if (array[i] === true){
+            newValue = true;
+            continue
+        }
+      newValue = func(array[i],newValue);
+    }
+    return newValue;
+}
+
+*/
